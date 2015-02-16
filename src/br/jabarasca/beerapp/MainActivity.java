1656a5c6935eb3c1d6994222f98db40d94015017
Filipe Borges <filipe_bkc@yahoo.com.br>
2015-02-16 17:44:56 -0200

    MainActivity refactored

--- src/br/jabarasca/beerapp/MainActivity.java
+++ src/br/jabarasca/beerapp/MainActivity.java
@@ -8,20 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.Toast;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends Activity {
 
-	LayoutInflater inflater;
-	OnClickListener action = new OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			Toast.makeText(getApplicationContext(), "Opção não implementada!", Toast.LENGTH_SHORT).show();
-		}
-	};//End OnClickListener.
+	final String OPTIONS_1 = "Buscar Cerveja";
+	final int OPTIONS_1_POSITION = 0;
 	
+	LayoutInflater inflater;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,30 +28,14 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.main_activity);
 		inflater = (LayoutInflater)this.getSystemService(LAYOUT_INFLATER_SERVICE);
 		
+		ListView listView = (ListView)findViewById(R.id.mainListView);
+
 		this.setTopBarLayout((LinearLayout)this.findViewById(R.id.mainLinearLayout));
-		
-		this.setScrollViewItem((LinearLayout)this.findViewById(R.id.mainScrollViewLinearLayout), R.id.scrollViewItemRelativeLayout1, R.drawable.item_list, R.string.txtViewItem1);
-		this.setScrollViewItem((LinearLayout)this.findViewById(R.id.mainScrollViewLinearLayout), R.id.scrollViewItemRelativeLayout2, R.drawable.item_list, R.string.txtViewItem2);
-		this.setScrollViewItem((LinearLayout)this.findViewById(R.id.mainScrollViewLinearLayout), R.id.scrollViewItemRelativeLayout3, R.drawable.item_list, R.string.txtViewItem3);
-		this.setScrollViewItem((LinearLayout)this.findViewById(R.id.mainScrollViewLinearLayout), R.id.scrollViewItemRelativeLayout4, R.drawable.item_list, R.string.txtViewItem4);
-		this.setScrollViewItem((LinearLayout)this.findViewById(R.id.mainScrollViewLinearLayout), R.id.scrollViewItemRelativeLayout5, R.drawable.item_list, R.string.txtViewItem5);
-		this.setScrollViewItem((LinearLayout)this.findViewById(R.id.mainScrollViewLinearLayout), R.id.scrollViewItemRelativeLayout6, R.drawable.item_list, R.string.txtViewItem6);
-		this.setScrollViewItem((LinearLayout)this.findViewById(R.id.mainScrollViewLinearLayout), R.id.scrollViewItemRelativeLayout7, R.drawable.item_list, R.string.txtViewItem7);
-		this.setScrollViewItem((LinearLayout)this.findViewById(R.id.mainScrollViewLinearLayout), R.id.scrollViewItemRelativeLayout8, R.drawable.item_list, R.string.txtViewItem8);
-		this.setScrollViewItem((LinearLayout)this.findViewById(R.id.mainScrollViewLinearLayout), R.id.scrollViewItemRelativeLayout9, R.drawable.item_list, R.string.txtViewItem9);
-		
-		
-		this.addListenerOnScrollViewItem((RelativeLayout)this.findViewById(R.id.scrollViewItemRelativeLayout1), action);
-		this.addListenerOnScrollViewItem((RelativeLayout)this.findViewById(R.id.scrollViewItemRelativeLayout2), action);
-		this.addListenerOnScrollViewItem((RelativeLayout)this.findViewById(R.id.scrollViewItemRelativeLayout3), action);
-		this.addListenerOnScrollViewItem((RelativeLayout)this.findViewById(R.id.scrollViewItemRelativeLayout4), action);
-		this.addListenerOnScrollViewItem((RelativeLayout)this.findViewById(R.id.scrollViewItemRelativeLayout5), action);
-		this.addListenerOnScrollViewItem((RelativeLayout)this.findViewById(R.id.scrollViewItemRelativeLayout6), action);
-		this.addListenerOnScrollViewItem((RelativeLayout)this.findViewById(R.id.scrollViewItemRelativeLayout7), action);
-		this.addListenerOnScrollViewItem((RelativeLayout)this.findViewById(R.id.scrollViewItemRelativeLayout8), action);
-		this.addListenerOnScrollViewItem((RelativeLayout)this.findViewById(R.id.scrollViewItemRelativeLayout9), action);
-	
-		this.addListenerOnTopBar((RelativeLayout)this.findViewById(R.id.topBarRelativeLayout), action);
+		this.setTopBarMoreOptsIconListener((ImageView)this.findViewById(R.id.topBarImgViewMoreOpts));
+		this.setTopBarFavListIconListener((ImageView)this.findViewById(R.id.topBarImgViewFavList));
+		this.setTopBarSetFavIconListener((ImageView)this.findViewById(R.id.topBarImgViewSetFav));
+		this.setListViewAdapter(listView);
+		this.setListViewItemsListener((ListView)this.findViewById(R.id.mainListView));
 	}
 
 	private void setTopBarLayout(LinearLayout linearLayout) {
@@ -60,33 +43,60 @@ public class MainActivity extends Activity {
 		linearLayout.addView(topBarLayout, 0);
 	}
 	
-	//Rever esse método.
-	private void addListenerOnTopBar(RelativeLayout topBarRelativeLayout, OnClickListener action) {
-		ImageView imgViewMoreOptsIcon = (ImageView)topBarRelativeLayout.getChildAt(1);
-		ImageView imgViewFavListIcon = (ImageView)topBarRelativeLayout.getChildAt(2);
-		ImageView imgViewSetFavIcon = (ImageView)topBarRelativeLayout.getChildAt(3);
+	private void setTopBarSetFavIconListener(ImageView topBarSetFavImgView) {
+		OnClickListener action = new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "Favoritar não implementado!", Toast.LENGTH_SHORT).show();
+			}
+		};
 		
-		imgViewMoreOptsIcon.setOnClickListener(action);
-		imgViewFavListIcon.setOnClickListener(action);
-		imgViewSetFavIcon.setOnClickListener(action);
+		topBarSetFavImgView.setOnClickListener(action);
 	}
 	
-	private void addListenerOnScrollViewItem(RelativeLayout scrollViewItemRelativeLayout, OnClickListener action) {
-		ImageView imgViewItem = (ImageView)scrollViewItemRelativeLayout.getChildAt(0);
-		imgViewItem.setOnClickListener(action);
-		TextView txtViewItem = (TextView)scrollViewItemRelativeLayout.getChildAt(1);
-		txtViewItem.setOnClickListener(action);
+	private void setTopBarFavListIconListener(ImageView topBarFavListImgView) {
+		OnClickListener action = new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "Lista de Fav. não implementado!", Toast.LENGTH_SHORT).show();
+			}
+		};
+		
+		topBarFavListImgView.setOnClickListener(action);
 	}
 	
-	private void setScrollViewItem(LinearLayout scrollViewLinearLayout, int idScrollViewItemRelativeLayout, int idImgViewItemBackground,  int idTxtViewItemString) {
-		RelativeLayout itemRelativeLayout = (RelativeLayout)inflater.inflate(R.layout.scrow_view_item, null);
-		itemRelativeLayout.setId(idScrollViewItemRelativeLayout);
-		ImageView imgViewItemBackground = (ImageView)itemRelativeLayout.getChildAt(0);
-		imgViewItemBackground.setImageResource(idImgViewItemBackground);
-		TextView itemTxtView = (TextView)itemRelativeLayout.getChildAt(1);
-		itemTxtView.setText(idTxtViewItemString);
-		scrollViewLinearLayout.addView(itemRelativeLayout);
+	private void setTopBarMoreOptsIconListener(ImageView topBarMoreOptsImgView) {
+		OnClickListener action = new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "Mais Opções não implementado!", Toast.LENGTH_SHORT).show();
+			}
+		};
+		
+		topBarMoreOptsImgView.setOnClickListener(action);
 	}
+	
+	private void setListViewAdapter(ListView listView) {
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_view_item, R.id.textViewListItem);
+		arrayAdapter.add(OPTIONS_1);
+		listView.setAdapter(arrayAdapter);
+	}
+	
+	private void setListViewItemsListener(ListView listView) {
+		OnItemClickListener listViewItemAction = new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> listView, View listItem, int itemPosition,
+					long itemRowId) {
+				switch(itemPosition) {
+					case OPTIONS_1_POSITION:
+						Toast.makeText(getApplicationContext(), OPTIONS_1+" não implementada!", Toast.LENGTH_SHORT).show();
+						break;
+				}
+			}
+		};
+		
+		listView.setOnItemClickListener(listViewItemAction);
+	}//End setListViewItemsListener.
 		
 	/*@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
